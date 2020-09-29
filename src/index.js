fb233bf9b262cdb8c171c187d0d606c8133dfa95
Hotinec Artiom <hotinec2@gmail.com>
2020-09-29 14:06:13 +0300

    feat: resolve min, max avg array mthods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let result = array[0];
+    array.forEach((element) => {
+        if (element < result) {
+            result = element;
+        }
+    });
 
-exports.max = function max (array) {
-  return 0;
-}
+    return result;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    let result = array[0];
+    array.forEach((element) => {
+        if (element > result) {
+            result = element;
+        }
+    });
+
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+};
